49f067bba55342b83a1432822371041f71abaaf1
Darío Sebastián Zárate Ceballlos <zdario488@gmail.com>
2024-05-11 00:22:38 -0600

    Terminando proyecto

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Formulario from './components/Formulario.vue'
+import Clima from './components/Clima.vue'
+import Spínner from './components/Spínner.vue'
+import Alerta from './components/Alerta.vue'
+import useClima from './composables/useClima'
+
+const { obtenerClima, clima, mostrarClima, cargando, error } = useClima()
+</script>
+
+<template>
+	<h1 class="titulo">Buscador de clima</h1>
+	<div class="contenedor buscador-clima">
+		<Formulario @obtener-clima="obtenerClima" />
+		<Spínner v-if="cargando" />
+		<Alerta v-if="error">{{ error }}</Alerta>
+		<Clima :clima="clima" v-if="mostrarClima" />
+	</div>
+</template>
